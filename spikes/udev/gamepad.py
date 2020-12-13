45057a540da45df8579dd7be000bc89342a7b6b0
Ian Glover <ian.glover@cydar.co.uk>
2020-12-13 22:33:42 +0000

    Code to read the gamepad key events.

--- spikes/udev/gamepad.py
+++ spikes/udev/gamepad.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 import pyudev as udev
+import enum
+import struct
 
 
 def get_gamepad_devices(ctx: udev.Context = None):
@@ -7,7 +9,7 @@ def get_gamepad_devices(ctx: udev.Context = None):
     if not ctx:
         ctx = udev.Context()
 
-    # Internal " is needed
+    # Internal " is needed for match to succeed.
     device_name = '"8BitDo Zero 2 gamepad Keyboard"'
     devices = list(ctx.list_devices(subsystem='input',
                                     NAME=device_name))
@@ -19,6 +21,49 @@ def get_gamepad_devices(ctx: udev.Context = None):
     return devices
 
 
+class PadKeys(enum.Enum):
+    """Input codes for gamepad keys."""
+    # Experimentally determined.
+    left_shoulder = 37
+    right_shoulder = 50
+    cross_up = 46
+    cross_left = 18
+    cross_right = 33
+    cross_down = 32
+    button_select = 49
+    button_start = 24
+    button_x = 35
+    button_y = 23
+    button_a = 34
+    button_b = 36
+
+
+class KeyAction(enum.Enum):
+    down = 1
+    repeat = 2
+    up = 0
+
+
+def read_device(dev_path: str):
+    # From /usr/include/linux/input.h
+    struct_input_event_format = 'llHHI'
+    struct_input_event_size = struct.calcsize(struct_input_event_format)
+
+    # From /usr/include/linux/input-event-codes.h
+    ev_key = 0x01
+    dev_file = open(dev_path, "rb")
+    try:
+        event = dev_file.read(struct_input_event_size)
+        while event:
+            (_, _, evt_type, code, value) = struct.unpack(struct_input_event_format, event)
+            if evt_type == ev_key:
+                print("%s : %s" % (PadKeys(code), KeyAction(value)))
+
+            event = dev_file.read(struct_input_event_size)
+    finally:
+        dev_file.close()
+
+
 def main():
     devices = get_gamepad_devices()
     if devices:
@@ -26,6 +71,9 @@ def main():
             print(device)
     else:
         print('No gamepad connected.')
+        return
+
+    read_device(devices.pop())
 
 
 if __name__ == '__main__':
